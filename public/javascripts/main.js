66b0d367b8595b7e7c4ed51d27c12ec10c6a4eab
Mike Jung <themikejung@gmail.com>
2022-05-25 12:57:27 -0700

    Placing modules in modules folder

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,6 @@
-import {DataManager} from './dataManager.js';
-import {DisplayManager} from './displayManager.js';
-import {ContactsManager} from './contactsManager.js';
+import {DataManager} from './modules/dataManager.js';
+import {DisplayManager} from './modules/displayManager.js';
+import {ContactsManager} from './modules/contactsManager.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const App = new ContactsManager( new DataManager(), new DisplayManager());
